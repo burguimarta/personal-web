2d52f0164ce3a5f8a088c30c07b410987dfa0ee5
Marta Torres Burguillo <99444191+burguimarta@users.noreply.github.com>
2022-06-06 11:45:59 +0200

    Update: Bio.js

---
+++ components/Bio.js
@@ -0,0 +1,20 @@
+
+
+export default function Bio() {
+    return (
+        <>
+            <h2 className="bio_title">
+                Sobre mi:
+            </h2>
+
+            <span className="bio_info">
+            
+            Inquieta, si hay una palabra que me define es esa. Me encanta aprender, siempre he sido así, me apasiona aprender cosas nuevas, tener retos que me hagan acostarme pensando en que he hecho algo nuevo.
+            He estudiado muchos ámbitos, biología, diseño (aunque este me inspiraba, nunca encontré mi lugar), trabajo social… Pero nunca he encontrado mi camino, hasta ahora. Decidí reinventarme en Adalab, y con ello he descubierto en la programación lo que realmente me gusta, porque aquí nunca paras de aprender y creo que eso es lo que más me mueve. 
+            Un dato curioso sobre mí, es que soy muy polifacética y me encanta escribir y leer, o tomarme un café en cualquier terraza y disfrutar del sol.
+
+            </span>
+        
+        </>
+    )
+};
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
 import Head from 'next/head'
+<<<<<<< Updated upstream
 import { useState } from 'react'
 
 import PersonalData from '../components/PersonalData'
 import Porfolio from '../components/Portfolio'
+=======
+import Bio from '../components/Bio'
+
+export default function Home() {
+  return (
+    <div className="container">
+      
+      <Bio></Bio>
+      
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+>>>>>>> Stashed changes
 
 import PortfolioItems from '../data/portfolio.json'
 
